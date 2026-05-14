12b49b9c07aa56f9185365ca0e501fcd39bc5684
jason-s13r <jason@js.org.nz>
2026-05-14 23:59:45 +1200

    Timer component with parent level start/stop buttons.

--- src/App.js
+++ src/App.js
@@ -1,77 +1,105 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import React from 'react';
 import './style.css';
 
-export default function App() {
+const format = duration => {
+  return new Intl.DurationFormat('en-NZ', {
+    style: 'digital',
+    hoursDisplay: 'auto',
+    minutesDisplay: 'always',
+    secondsDisplay: 'always',
+    minutesDisplay: 'always',
+  }).format(duration.round({ largestUnit: 'hours', smallestUnit: 'milliseconds' }));
+}
+
+function Timer({ Hz, running, reset }) {
   const [startTime, setStartTime] = useState(undefined);
   const [endTime, setEndTime] = useState(undefined);
   const [totalDuration, setTotalDuration] = useState(new Temporal.Duration());
   const [pausedDuration, setPausedDuration] = useState(new Temporal.Duration());
   const [intervalId, setIntervalId] = useState(-1);
 
-  const formatter = new Intl.DurationFormat('en-NZ', {
-    style: 'digital',
-    hoursDisplay: 'auto',
-    secondsDisplay: 'always',
-    minutesDisplay: 'always',
-  });
-
   const start = () => {
     const now = Temporal.Now.instant();
+    setEndTime(now);
     if (!startTime) {
       setStartTime(now);
     }
     if (endTime) {
       setPausedDuration((paused) => paused.add(now.since(endTime)));
     }
-    setIntervalId(setInterval(() => setEndTime(Temporal.Now.instant()), 1000 / 24));
-  };
+
+    setIntervalId(setInterval(() => setEndTime(Temporal.Now.instant()), 1000 / Hz));
+  }
 
   const stop = () => {
-    setEndTime(Temporal.Now.instant());
+    const now = Temporal.Now.instant();
+    setEndTime(now);
     clearInterval(intervalId);
     setIntervalId(-1);
   };
 
-  const reset = () => {
-    setStartTime(intervalId === -1 ? undefined : Temporal.Now.instant());
+  const onReset = () => {
+    setStartTime(!running ? undefined : Temporal.Now.instant());
     setEndTime(undefined);
     setTotalDuration(new Temporal.Duration());
     setPausedDuration(new Temporal.Duration());
   };
 
+  useEffect(() => running ? start() : stop(), [running]);
+  useEffect(() => onReset(), [reset]);
   useEffect(() => {
     setTotalDuration(
       endTime?.since(startTime ?? endTime, {
+        largestUnit: 'hours',
         smallestUnit: 'microseconds',
       }) ?? new Temporal.Duration()
     );
   }, [endTime, startTime]);
 
+
   return (
     <div>
-      <h1>{formatter.format(totalDuration.subtract(pausedDuration))}</h1>
-      {intervalId === -1 ? (
-        <button onClick={start}>start</button>
-      ) : (
-        <button onClick={stop}>stop</button>
-      )}
-      <button onClick={reset}>reset</button>
+      <big>{format(totalDuration.subtract(pausedDuration))}</big>
+      <small>@ {Hz} Hz</small>
       <pre>
         <div>
           start time:{' '}
-          {startTime && startTime !== endTime ? startTime.toString() : '-'}
+          {startTime ? startTime.toString() : '-'}
         </div>
         <div>
           end time:{' '}
           {endTime && startTime !== endTime ? endTime.toString() : '-'}
         </div>
-        <div>+++ elapsed: {formatter.format(totalDuration)}</div>
-        <div>-- paused: {formatter.format(pausedDuration)}</div>
+        <div>+++ elapsed: {format(totalDuration)}</div>
+        <div>-- paused: {format(pausedDuration)}</div>
         <div>
-          = timed: {formatter.format(totalDuration.subtract(pausedDuration))}
+          = timed: {format(totalDuration.subtract(pausedDuration))}
         </div>
       </pre>
+    </div>
+  );
+}
+
+
+export default function App() {
+  const [running, setRunning] = useState(false);
+  const [reset, setReset] = useState(0);
+
+  const onStart = () => setRunning(true);
+  const onStop = () => setRunning(false);
+  const onReset = () => setReset(r => r + 1);
+
+  return (
+    <div>
+      <Timer Hz={12} running={running} reset={reset} />
+      <Timer Hz={24} running={running} reset={reset} />
+      <Timer Hz={1} running={running} reset={reset} />
+      <Timer Hz={2} running={running} reset={reset} />
+      {running ?
+        <button onClick={onStop}>stop</button> :
+        <button onClick={onStart}>start</button>}
+      <button onClick={onReset}>reset</button>
     </div>
   );
 }
